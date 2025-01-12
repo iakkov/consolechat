4712e33aa3f276e653165d730605644dd2501ad4
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-01-12 14:42:49 +0300

    Дополнения в DatabaseManager

--- Server/src/main/java/ru/project/database/DatabaseManager.java
+++ Server/src/main/java/ru/project/database/DatabaseManager.java
@@ -12,6 +12,7 @@ public class DatabaseManager {
                     CREATE TABLE IF NOT EXISTS users (
                     id SERIAL PRIMARY KEY,
                     username TEXT NOT NULL UNIQUE,
+                    login TEXT NOT NULL UNIQUE,
                     password TEXT NOT NULL,
                     role TEXT NOT NULL
                 );
@@ -20,5 +21,53 @@ public class DatabaseManager {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    public String authenticate(String login, String hashedPassword) {
+        String query = "SELECT username FROM users WHERE login = ? AND password = ?";
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, login);
+            pstmt.setString(2, hashedPassword);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getString("username");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public boolean registerUser(String username, String login, String hashedPassword, String role) {
+        String query = "INSERT INTO users (username, login, password, role) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, username);
+            pstmt.setString(2, login);
+            pstmt.setString(3, hashedPassword);
+            pstmt.setString(4, role.toUpperCase());
+            pstmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            if (e.getMessage().contains("UNIQUE")) {
+                System.out.println("Логин или имя пользователя уже существуют.");
+            } else {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+    public String getUserRole(String username) {
+        String query = "SELECT role FROM users WHERE username = ?";
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, username);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getString("role");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
