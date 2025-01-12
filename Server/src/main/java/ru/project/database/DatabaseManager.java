75ca6a797d723124eba8b1c27dc86d49f488d221
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-01-12 16:03:00 +0300

    Добавление сходных методов как в классе inMemoryAuthenticator, а также доработка метода registerUser

--- Server/src/main/java/ru/project/database/DatabaseManager.java
+++ Server/src/main/java/ru/project/database/DatabaseManager.java
@@ -1,9 +1,11 @@
 package ru.project.database;
 
+import ru.project.Role;
 import java.sql.*;
 
 public class DatabaseManager {
-    private static final String DB_URL = "jdbc:sqlite:chat.db";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres?currentSchema=public&user=postgres&password=admin";
+
 
     public DatabaseManager() {
         try (Connection conn = DriverManager.getConnection(DB_URL);
@@ -22,12 +24,12 @@ public class DatabaseManager {
             e.printStackTrace();
         }
     }
-    public String authenticate(String login, String hashedPassword) {
+    public String authenticate(String login, String password) {
         String query = "SELECT username FROM users WHERE login = ? AND password = ?";
         try (Connection conn = DriverManager.getConnection(DB_URL);
              PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, login);
-            pstmt.setString(2, hashedPassword);
+            pstmt.setString(2, password);
             ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 return rs.getString("username");
@@ -37,14 +39,14 @@ public class DatabaseManager {
         }
         return null;
     }
-    public boolean registerUser(String username, String login, String hashedPassword, String role) {
+    public boolean registerUser(String username, String login, String password, Role role) {
         String query = "INSERT INTO users (username, login, password, role) VALUES (?, ?, ?, ?)";
         try (Connection conn = DriverManager.getConnection(DB_URL);
              PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, username);
             pstmt.setString(2, login);
-            pstmt.setString(3, hashedPassword);
-            pstmt.setString(4, role.toUpperCase());
+            pstmt.setString(3, password);
+            pstmt.setString(4, String.valueOf(role));
             pstmt.executeUpdate();
             return true;
         } catch (SQLException e) {
@@ -64,6 +66,45 @@ public class DatabaseManager {
             ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 return rs.getString("role");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public boolean isUsernameAlreadyExist(String username) {
+        String query = "SELECT 1 FROM users WHERE username = ?";
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, username);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean isLoginAlreadyExist(String login) {
+        String query = "SELECT 1 FROM users WHERE login = ?";
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, login);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public String getUsernameByLoginAndPassword(String login, String password) {
+        String query = "SELECT username FROM users WHERE username = ? AND password = ?";
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, login);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getString("username");
             }
         } catch (SQLException e) {
             e.printStackTrace();
