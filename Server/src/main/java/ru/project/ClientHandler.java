002f4f4214953da755662c7dcfd33d3f38eb508f
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-02-16 22:56:21 +0300

    Чистка кода

--- Server/src/main/java/ru/project/ClientHandler.java
+++ Server/src/main/java/ru/project/ClientHandler.java
@@ -2,19 +2,17 @@ package ru.project;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
 public class ClientHandler {
-    private Socket socket;
-    private Server server;
-    private DataInputStream in;
-    private DataOutputStream out;
+    private final Socket socket;
+    private final Server server;
+    private final DataInputStream in;
+    private final DataOutputStream out;
     private String username;
     private Role role;
 
@@ -29,10 +27,11 @@ public class ClientHandler {
                 System.out.println("Клиент подключился " + socket.getPort());
                 //Цикл логина
                 while (true) {
-                    sendMsg("Для начала работы надо пройти аутентификацию или регистрацию\n" +
-                            "Формат команды для аутентификации: /log\n" +
-                            "Формат команды для регистрации: /reg\n" +
-                            "Для выхода используйте комманду /exit");
+                    sendMsg("""
+                            Для начала работы надо пройти аутентификацию или регистрацию
+                            Формат команды для аутентификации: /log
+                            Формат команды для регистрации: /reg
+                            Для выхода используйте комманду /exit""");
                     String message = in.readUTF();
                     if (message.startsWith("/")) {
                         if (message.equals("/exit")) {
@@ -55,13 +54,15 @@ public class ClientHandler {
                         }
                         if (message.equalsIgnoreCase("/reg")) {
                             String[] regData = new String[3];
-                            sendMsg("Введите никнейм.\n" +
-                                    "Никнейм не должен содержать более 20 символов, и не менее 5 символов\n" +
-                                    "Можно использовать цифры, латинские буквы и знаки \"_\" и \"-\"");
+                            sendMsg("""
+                                    Введите никнейм.
+                                    Никнейм не должен содержать более 20 символов, и не менее 5 символов
+                                    Можно использовать цифры, латинские буквы и знаки "_" и "-\"""");
                             regData[0] = in.readUTF();
-                            sendMsg("Введите логин.\n" +
-                                    "Логин не должен содержать более 20 символов, и не менее 5 символов\n" +
-                                    "Можно использовать цифры, латинские буквы и знаки \"_\" и \"-\"");
+                            sendMsg("""
+                                    Введите логин.
+                                    Логин не должен содержать более 20 символов, и не менее 5 символов
+                                    Можно использовать цифры, латинские буквы и знаки "_" и "-\"""");
                             regData[1] = in.readUTF();
                             sendMsg("Введите пароль.\n" +
                                     "Пароль должен содержать от 5 до 20 символов и обязательно включать в себя любой специальный символ.");
