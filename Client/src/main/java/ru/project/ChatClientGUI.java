c1a1897e7c03448f68ba5009c49dda4d3b9faccf
iakovlysenko <lysenko_iakov@yahoo.com>
2025-02-20 00:35:56 +0300

    Интерфейс: вывод сообщений

--- Client/src/main/java/ru/project/ChatClientGUI.java
+++ Client/src/main/java/ru/project/ChatClientGUI.java
@@ -1,18 +1,28 @@
 package ru.project;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ChatClientGUI extends Application {
+    private TextArea chatArea;
+    private TextField inputField;
+    private Button sendButton;
 
     @Override
     public void start(Stage primaryStage) {
+        chatArea = new TextArea();
+        chatArea.setEditable(false);
+        chatArea.setWrapText(true);
+
         TextField inputField = new TextField();
         inputField.setPromptText("Введите сообщение...");
+
         Button sendButton = new Button("Отправить");
+
+        sendButton.setOnAction(e -> sendMessage());
+
         VBox root = new VBox(10, inputField, sendButton);
         Scene scene = new Scene(root, 400, 300);
 
@@ -23,5 +33,13 @@ public class ChatClientGUI extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void sendMessage() {
+        String message = inputField.getText().trim();
+        if (!message.isEmpty()) {
+            chatArea.appendText("Вы: " + message + "\n");
+            inputField.clear();
+        }
     }
 }
