2a61f8f0a7176111936f2f58a756701d6fc5d06b
iakovlysenko <lysenko_iakov@yahoo.com>
2025-02-20 00:46:21 +0300

    Интерфейс: подключение к серверу

--- Client/src/main/java/ru/project/ChatClientGUI.java
+++ Client/src/main/java/ru/project/ChatClientGUI.java
@@ -1,14 +1,22 @@
 package ru.project;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ChatClientGUI extends Application {
     private TextArea chatArea;
     private TextField inputField;
     private Button sendButton;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
 
     @Override
     public void start(Stage primaryStage) {
@@ -29,6 +37,8 @@ public class ChatClientGUI extends Application {
         primaryStage.setTitle("Чат-клиент");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        connectToServer();
     }
 
     public static void main(String[] args) {
@@ -40,6 +50,27 @@ public class ChatClientGUI extends Application {
         if (!message.isEmpty()) {
             chatArea.appendText("Вы: " + message + "\n");
             inputField.clear();
+        }
+    }
+    private void connectToServer() {
+        try {
+            socket = new Socket("localhost", 8189);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            chatArea.appendText("Подключено к серверу\n");
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String message = in.readUTF();
+                        Platform.runLater(() -> chatArea.appendText(message + "\n"));
+                    }
+                } catch (Exception e) {
+                    Platform.runLater(() -> chatArea.appendText(e.getMessage() + "\n"));
+                }
+            }).start();
+        } catch (Exception e) {
+            chatArea.appendText(e.getMessage() + "\n");
         }
     }
 }
